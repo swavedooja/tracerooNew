d29f8a1955fe133998b3dbdf1f3be71315b5d502
Swapnil Pande <swapnilpande@example.com>
2026-02-04 16:22:15 +0530

    Fix: Switch to HashRouter for GitHub Pages compatibility

--- ilms-frontend/src/index.jsx
+++ ilms-frontend/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import App from './App';
 import theme from './theme';
@@ -11,9 +11,9 @@ root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
